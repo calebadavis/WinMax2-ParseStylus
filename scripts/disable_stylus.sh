e5c615e901d0abc5be98dc34ef4956695bea852f
Caleb Davis <caleb.davis@gmail.com>
2025-01-12 23:15:33 -0500

    Sample bash script to invoke stylus disable

---
+++ scripts/disable_stylus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+java_exe="/usr/bin/java"
+jar_file="/home/cdavis/lib/ParseStylus.jar"
+input_devices_file="/proc/bus/input/devices"
+i2c_path=$($java_exe -jar $jar_file $input_devices_file)
+echo "$i2c_path"
+echo 1 | sudo tee "$i2c_path/inhibited"
+
